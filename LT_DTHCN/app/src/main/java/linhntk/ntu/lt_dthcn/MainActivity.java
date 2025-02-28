d6df39d86816eb0056f6b67560264c8a79d8c8b8
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-02-28 20:02:04 +0700

    xong lab điểm cộng _ hehe
    xong lab điểm cộng _ hehe

--- LT_DTHCN/app/src/main/java/linhntk/ntu/lt_dthcn/MainActivity.java
+++ LT_DTHCN/app/src/main/java/linhntk/ntu/lt_dthcn/MainActivity.java
@@ -1,6 +1,8 @@
 package linhntk.ntu.lt_dthcn;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void XulyNhan(View view){
+        EditText editTextChieudai = findViewById(R.id.editCD);
+        EditText editTextChieurong = findViewById(R.id.editCD);
+        EditText editTextKetqua = findViewById(R.id.editTextNumber);
+        String CD = editTextChieudai.getText().toString();
+        String CR = editTextChieurong.getText().toString();
+        int c_dai = Integer.parseInt(CD);
+        int c_rong = Integer.parseInt(CR);
+        int dientich = c_dai * c_rong;
+        String DT = String.valueOf(dientich);
+        editTextKetqua.setText(DT);
     }
 }
