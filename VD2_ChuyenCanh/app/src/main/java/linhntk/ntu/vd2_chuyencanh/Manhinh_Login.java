01399b5f17673c3568a30601aaedbf7590e4f71f
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-03-10 16:54:40 +0700

    hoan thanh Logic dang nhap
    hoan thanh Logic dang nhap

--- VD2_ChuyenCanh/app/src/main/java/linhntk/ntu/vd2_chuyencanh/Manhinh_Login.java
+++ VD2_ChuyenCanh/app/src/main/java/linhntk/ntu/vd2_chuyencanh/Manhinh_Login.java
@@ -1,6 +1,10 @@
 package linhntk.ntu.vd2_chuyencanh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Manhinh_Login extends AppCompatActivity {
-
+    public Button btnDn;
+    public EditText edtInputUser, edtInputPass,edtInputMail;
+    public String getedtInputUser(){
+        return edtInputUser.getText().toString();
+    }
+    public String getedtInputPass(){
+        return  edtInputPass.getText().toString();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_manhinh_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnDn = findViewById(R.id.btnDn);
+        edtInputUser = findViewById(R.id.edtInputUser);
+        edtInputPass = findViewById(R.id.edtInputPass);
+        edtInputMail = findViewById(R.id.edtInputMail);
+        btnDn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(getedtInputUser().equals("khanhlinh") && getedtInputPass().equals("123")){
+                    Intent intent = new Intent(Manhinh_Login.this, ManhinhChaomung.class);
+                    intent.putExtra("123",getedtInputUser());
+                    startActivity(intent);
+                }
+            }
         });
     }
 }
