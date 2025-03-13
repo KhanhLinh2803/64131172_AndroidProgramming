5f3f25629990afedd76da346c55c760c634df387
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-03-13 08:22:28 +0700

    tạo ds BH

--- LT_ListView_BTS/app/src/main/java/linhntk/ntu/lt_listview_bts/ManDN.java
+++ LT_ListView_BTS/app/src/main/java/linhntk/ntu/lt_listview_bts/ManDN.java
@@ -1,6 +1,11 @@
 package linhntk.ntu.lt_listview_bts;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ManDN extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class ManDN extends AppCompatActivity {
+    ListView lvDSBH;
+   public ArrayList<String> Dsbh = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_man_dn);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        lvDSBH = findViewById(R.id.lvDSBH);
+        Dsbh.add("Boy with Love");
+        Dsbh.add("DNA");
+        Dsbh.add("Idol");
+        Dsbh.add("Fake love");
+        //khai bao adapter
+        ArrayAdapter<String> adapterDsbh = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, Dsbh);
+        //gan adapter cho listview
+        lvDSBH.setAdapter(adapterDsbh);
+        //gan su kien
+        lvDSBH.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //
+                String Baihatduochon = Dsbh.get(position);
+
+            }
         });
     }
 }
