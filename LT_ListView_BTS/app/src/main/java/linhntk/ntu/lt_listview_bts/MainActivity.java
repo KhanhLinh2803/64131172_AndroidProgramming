ed9e3470a20cb786fc274a2883a4db0a361338ed
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-03-13 08:08:24 +0700

    làm tiếp màn hình DSBH

--- LT_ListView_BTS/app/src/main/java/linhntk/ntu/lt_listview_bts/MainActivity.java
+++ LT_ListView_BTS/app/src/main/java/linhntk/ntu/lt_listview_bts/MainActivity.java
@@ -1,6 +1,10 @@
 package linhntk.ntu.lt_listview_bts;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,32 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText txtInputTK, txtInputMK;
+    Button btnDN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txtInputMK =findViewById(R.id.txtInputMK);
+        txtInputTK =findViewById(R.id.txtInputTK);
+        btnDN = findViewById(R.id.btnDN);
+        //xu ly su kien lang nghe
+        btnDN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(gettxtInputTK().equals("Khanh Linh") && gettxtInputMK().equals("1")){
+                    Intent intent = new Intent(MainActivity.this, ManDN.class);
+                    startActivity(intent);
+                }
+            }
         });
+    }
+    //lay gia tri nhap vao cua txtInputTK
+    public String gettxtInputTK(){
+        return txtInputTK.getText().toString();
+    }
+    public  String gettxtInputMK(){
+        return  txtInputMK.getText().toString();
     }
 }
