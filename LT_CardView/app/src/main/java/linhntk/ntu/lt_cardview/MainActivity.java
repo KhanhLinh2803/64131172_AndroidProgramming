b074fbd9ac5f8244127e7933b139f4a35f68f692
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-03-20 14:28:43 +0700

    CardView

---
+++ LT_CardView/app/src/main/java/linhntk/ntu/lt_cardview/MainActivity.java
@@ -0,0 +1,36 @@
+package linhntk.ntu.lt_cardview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnHome;
+    TextView txtListview;
+    ListView lsDS;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        btnHome = findViewById(R.id.btnHome);
+        txtListview = findViewById(R.id.txtListview);
+        lsDS = findViewById(R.id.lvDS);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Home.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
