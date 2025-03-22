30c10e13b66af7c591eb5d861a2f6d51eed6f768
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-03-22 09:56:49 +0700

    thêm Page

--- LT_Figma/app/src/main/java/linhntk/ntu/lt_figma/HomePage.java
+++ LT_Figma/app/src/main/java/linhntk/ntu/lt_figma/HomePage.java
@@ -12,18 +12,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage extends AppCompatActivity {
-    TextView textView5;
+    TextView textView5, textView3, textView6, textView7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
         textView5 = findViewById(R.id.textView5);
+        textView3 = findViewById(R.id.textView3);
+        textView6 = findViewById(R.id.textView6);
+        textView7 = findViewById(R.id.textView7);
 
         textView5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomePage.this, HomeMath.class);
+                startActivity(intent);
+            }
+        });
+        textView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, HomeScience.class);
+                startActivity(intent);
+            }
+        });
+        textView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, HomeMusic.class);
+                startActivity(intent);
+            }
+        });
+        textView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, HomePainting.class);
                 startActivity(intent);
             }
         });
