72b43e080ba74f0e7601c21c8abd53940bfe6f2a
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-03-28 01:45:39 +0700

    chu vi hình tròn

--- LT_GK/app/src/main/java/linhntk/ntu/lt_gk/Cau1_1.java
+++ LT_GK/app/src/main/java/linhntk/ntu/lt_gk/Cau1_1.java
@@ -1,6 +1,10 @@
 package linhntk.ntu.lt_gk;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,36 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Cau1_1 extends AppCompatActivity {
-
+    EditText edtBanKinh;
+    TextView txtKetQua;
+    Button btnTinhChuVi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cau11);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        edtBanKinh = findViewById(R.id.edtBanKinh);
+        txtKetQua = findViewById(R.id.txtKetQua);
+        btnTinhChuVi = findViewById(R.id.btnTinhChuVi);
+
+        btnTinhChuVi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tinhChuVi();
+            }
         });
+    }
+
+    private void tinhChuVi() {
+        String strBanKinh = edtBanKinh.getText().toString();
+
+        if (!strBanKinh.isEmpty()) {
+            double R = Double.parseDouble(strBanKinh);
+            double chuVi = 2 * Math.PI * R;
+
+            txtKetQua.setText("Chu vi: " + String.format("%.2f", chuVi));
+        } else {
+            txtKetQua.setText("Vui lòng nhập bán kính!");
+        }
     }
 }
