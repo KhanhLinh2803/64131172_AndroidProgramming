bfa0e15ba71a001c892eec9ceed632e6a71ee2a1
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-04-24 04:06:43 +0700

    toast.maketest

--- Ex_224/app/src/main/java/linhntk/ntu/ex_224/MainActivity.java
+++ Ex_224/app/src/main/java/linhntk/ntu/ex_224/MainActivity.java
@@ -1,19 +1,44 @@
 package linhntk.ntu.ex_224;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
+    BottomNavigationView bottomNa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //tim
+        bottomNa = findViewById(R.id.bot_menu);
+        //lang nghe
+        bottomNa.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int menuItemID = item.getItemId();
+                if (menuItemID == R.id.navHome)
+                    //thay fragment Home
+                    Toast.makeText(MainActivity.this, "Thay Home", Toast.LENGTH_SHORT).show();
+                else if (menuItemID == R.id.navSearch)
+                    Toast.makeText(MainActivity.this, "Thay Search", Toast.LENGTH_SHORT).show();
+                else if (menuItemID == R.id.navProfile)
+                    Toast.makeText(MainActivity.this, "Thay Profile", Toast.LENGTH_SHORT).show();
+                else return false;
+                return true;
+            }
+        });
 
     }
 }
