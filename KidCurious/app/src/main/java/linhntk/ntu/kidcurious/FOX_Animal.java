e139316981674b1db95cb4ed6b028cdc5a9db5e0
Nguyễn Thị Khánh Linh <trankhanh9791@gmail.com>
2025-05-02 23:49:46 +0700

    não cá vàng

--- KidCurious/app/src/main/java/linhntk/ntu/kidcurious/FOX_Animal.java
+++ KidCurious/app/src/main/java/linhntk/ntu/kidcurious/FOX_Animal.java
@@ -1,6 +1,10 @@
 package linhntk.ntu.kidcurious;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,29 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class FOX_Animal extends AppCompatActivity {
-
+    Button btnX1, btnD1;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_fox_animal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnD1 = findViewById(R.id.btnD1);
+        btnX1 = findViewById(R.id.btnX1);
+
+        btnX1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent btnx1 = new Intent(FOX_Animal.this, Animal.class);
+                startActivity(btnx1);
+            }
+        });
+        btnD1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
 }
